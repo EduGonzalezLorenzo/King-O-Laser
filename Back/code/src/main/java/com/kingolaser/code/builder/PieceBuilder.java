7187cae0ad535a0d8546bab4aa4329de3a3a0370
eduar <edugonzalezlorenzo@gmail.com>
2023-04-06 10:48:06 +0200

    Match, MatchBuilder and PieceBuilder implementation

---
+++ Back/code/src/main/java/com/kingolaser/code/builder/PieceBuilder.java
@@ -0,0 +1,17 @@
+package com.kingolaser.code.builder;
+
+import com.kingolaser.code.models.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PieceBuilder {
+    public static List<Piece> getStartPieceList() {
+        List<Piece> pieceList = new ArrayList<>();
+
+        
+
+        return pieceList;
+    }
+
+}
--- Back/code/src/main/java/com/kingolaser/code/models/Match.java
+++ Back/code/src/main/java/com/kingolaser/code/models/Match.java
@@ -15,4 +15,70 @@ public class Match {
     List<Piece> pieceList;
     Boolean isPublic;
     Boolean isOnline;
+    String status;
+//  Status list: NOT_READY, READY, PLAYER_ONE_TURN, PLAYER_TWO_TURN, PLAYER_ONE_WIN, PLAYER_TWO_WIN, DRAFT
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Player getPlayerOne() {
+        return playerOne;
+    }
+
+    public void setPlayerOne(Player playerOne) {
+        this.playerOne = playerOne;
+    }
+
+    public Player getPlayerTwo() {
+        return playerTwo;
+    }
+
+    public void setPlayerTwo(Player playerTwo) {
+        this.playerTwo = playerTwo;
+    }
+
+    public List<Piece> getPieceList() {
+        return pieceList;
+    }
+
+    public void setPieceList(List<Piece> pieceList) {
+        this.pieceList = pieceList;
+    }
+
+    public Boolean getPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(Boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public Boolean getOnline() {
+        return isOnline;
+    }
+
+    public void setOnline(Boolean online) {
+        isOnline = online;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
