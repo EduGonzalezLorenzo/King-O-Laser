97c8cd3b4eab5529d6ed94fdd21843fa629686da
eduar <edugonzalezlorenzo@gmail.com>
2023-04-08 12:33:29 +0200

    Match update

--- Back/code/src/main/java/com/telegame/code/models/Match.java
+++ Back/code/src/main/java/com/telegame/code/models/Match.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class Match {
-    public enum matchStatus {
+    public enum MatchStatus {
         WAITING, PLAYER_ONE_TURN, PLAYER_TWO_TURN, PLAYER_ONE_WIN, PLAYER_TWO_WIN, DRAFT
     }
 
@@ -20,78 +24,6 @@ public class Match {
     Player playerTwo;
     Board board;
     Boolean isPublic;
-    Boolean isOnline;
-    matchStatus status;
+    MatchStatus status;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Player getPlayerOne() {
-        return playerOne;
-    }
-
-    public void setPlayerOne(Player playerOne) {
-        this.playerOne = playerOne;
-    }
-
-    public Player getPlayerTwo() {
-        return playerTwo;
-    }
-
-    public void setPlayerTwo(Player playerTwo) {
-        this.playerTwo = playerTwo;
-    }
-
-    public Board getBoard() {
-        return board;
-    }
-
-    public void setBoard(Board board) {
-        this.board = board;
-    }
-
-    public Boolean getPublic() {
-        return isPublic;
-    }
-
-    public void setPublic(Boolean aPublic) {
-        isPublic = aPublic;
-    }
-
-    public Boolean getOnline() {
-        return isOnline;
-    }
-
-    public void setOnline(Boolean online) {
-        isOnline = online;
-    }
-
-    public matchStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(matchStatus status) {
-        this.status = status;
-    }
 }
