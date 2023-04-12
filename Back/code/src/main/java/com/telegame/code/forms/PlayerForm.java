affef6731a833bc411d0d489f06c9f3b4f8d2cc7
eduar <edugonzalezlorenzo@gmail.com>
2023-04-12 16:34:24 +0200

    Form error fixed. Now nothing can be null.

--- Back/code/src/main/java/com/telegame/code/forms/PlayerForm.java
+++ Back/code/src/main/java/com/telegame/code/forms/PlayerForm.java
@@ -1,6 +1,7 @@
 package com.telegame.code.forms;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,17 +11,22 @@ import lombok.Setter;
 public class PlayerForm {
     @Email
     @Size(min = 6, max = 30)
+    @NotNull
     String email;
 
     @Size(min = 8, max = 20)
+    @NotNull
     String password;
 
     @Size(min = 3, max = 20)
+    @NotNull
     String firstName;
 
     @Size(min = 3, max = 20)
+    @NotNull
     String lastName;
 
+    @NotNull
     @Size(min = 3, max = 10)
     String playerName;
 }
