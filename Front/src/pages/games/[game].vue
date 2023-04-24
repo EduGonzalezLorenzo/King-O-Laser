cc4863d73aeebca74fb19e7d7e6c924c4a3ac70c
Jhota <2002juanan@gmail.com>
2023-04-24 20:46:07 +0200

    Add grid comp and canvas draw

---
+++ Front/src/pages/games/[game].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>My Grid Component</h1>
+    <Grid />
+  </div>
+</template>
+  
+  <script lang="ts">
+  import Grid from '~/components/GridComp.vue';
+  
+  export default {
+    components: {
+      Grid
+    }
+  }
+  </script>
+  
