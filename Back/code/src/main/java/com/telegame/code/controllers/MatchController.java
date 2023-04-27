c1695719f8b45341946de6d6b571d435b64682ee
f.martinez <f.martinez@innovation.es>
2023-04-27 19:00:56 +0200

    Test

--- Back/code/src/main/java/com/telegame/code/controllers/MatchController.java
+++ Back/code/src/main/java/com/telegame/code/controllers/MatchController.java
@@ -39,8 +39,7 @@ public class MatchController {
     public MatchController(MatchService matchService) {
         this.matchService = matchService;
     }
-
-
+    
     @PostMapping("/match/kingolaser")
     public ResponseEntity<String> createMatch(@RequestBody MatchForm matchForm, HttpServletRequest request) {
 
