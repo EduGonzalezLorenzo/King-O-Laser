f84723d7252aa8aa8e92e9ff0858a54ba850e306
Eduardo González Lorenzo <95251418+ElTambores@users.noreply.github.com>
2023-05-02 19:32:05 +0200

    Interceptors (#16)
    Revisado

--- Back/code/src/main/java/com/telegame/code/CodeApplication.java
+++ Back/code/src/main/java/com/telegame/code/CodeApplication.java
@@ -1,13 +1,33 @@
 package com.telegame.code;
 
+import com.telegame.code.interceptors.TokenInterceptor;
+import com.telegame.code.interceptors.UserInterceptor;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@AllArgsConstructor
 @SpringBootApplication
-public class CodeApplication {
+public class CodeApplication implements WebMvcConfigurer {
+    TokenInterceptor tokenInterceptor;
+    UserInterceptor userInterceptor;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CodeApplication.class, args);
+    }
 
+    public void addInterceptors(InterceptorRegistry registry) {
+        //URLs where you need to be logged
+        registry.addInterceptor(tokenInterceptor)
+                .addPathPatterns("/game/**")
+                .addPathPatterns("/getPlayer")
+                .addPathPatterns("/update/**");
+
+        //URLs where you need to be NO logged
+        registry.addInterceptor(userInterceptor)
+                .addPathPatterns("/login")
+                .addPathPatterns("/signUp");
+    }
 }
