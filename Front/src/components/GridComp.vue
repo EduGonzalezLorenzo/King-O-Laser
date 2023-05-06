beaaf383b8d4b76b5e5ed9d7ac589288c312f966
f.martinez <f.martinez@innovation.es>
2023-05-06 09:19:46 +0200

    BoardPositions OK

--- Front/src/components/GridComp.vue
+++ Front/src/components/GridComp.vue
@@ -8,13 +8,15 @@
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue';
+import { Cell } from '~/types/Cell';
+import createCell from '~/utils/createCell';
 
 export default defineComponent({
   name: 'CanvasTable',
   setup() {
     const canvas = ref<HTMLCanvasElement | null>(null);
-    const canvasWidth = 600;
-    const canvasHeight = 800;
+    const canvasWidth = 1200;
+    const canvasHeight = 1500;
     const tableRows = 10;
     const tableColumns = 8;
 
@@ -22,12 +24,31 @@ export default defineComponent({
       const cellWidth = canvasWidth / tableColumns;
       const cellHeight = canvasHeight / tableRows;
 
-      for (let i = 0; i < tableRows; i++) {
-        for (let j = 0; j < tableColumns; j++) {
-          ctx.strokeStyle = 'black';
-          ctx.strokeRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+
+      
+      const board = new Array(tableColumns)
+
+      for (let x = 0; x < board.length; x++) {
+        board[x] = new Array(tableRows)
+        for (let y = 0; y < tableRows; y++) {
+          
+          board[x][y] = createCell(cellWidth, cellHeight, y, x, "#fff")
+          show(board[x][y])
         }
       }
+
+
+      function show(cell: Cell) {
+        ctx.beginPath();
+        ctx.rect(cell.posX*cellHeight, cell.posY*cellWidth, cell.height, cell.width);
+        ctx.stroke();
+        ctx.font = "30px Arial";
+        ctx.fillText(`${cell.posY}:${cell.posX}`, cell.posX*cellHeight +50, cell.posY*cellWidth +100)
+        ctx.fillStyle = 'rgba(150,150,255, 0.5)';
+        ctx.fill();
+      }
+
+      console.log({board})
     };
 
     onMounted(() => {
