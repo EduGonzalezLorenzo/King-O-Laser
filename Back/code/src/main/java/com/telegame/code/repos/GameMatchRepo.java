f84964e0b86d9a8f124df6c47eea93eeffd403f1
Eduardo González Lorenzo <edugonzalezlorenzo@gmail.com>
2023-05-07 17:40:02 +0200

    Create match

--- Back/code/src/main/java/com/telegame/code/repos/GameMatchRepo.java
+++ Back/code/src/main/java/com/telegame/code/repos/GameMatchRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MatchRepo extends JpaRepository<GameMatch, Long> {
+public interface GameMatchRepo extends JpaRepository<GameMatch, Long> {
 }
