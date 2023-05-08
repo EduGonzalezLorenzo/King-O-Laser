aac78c3d9cda74eb8fd08e2a5f5f1ff764ff123c
f.martinez <f.martinez@innovation.es>
2023-05-08 18:06:58 +0200

    Fix board error

--- Front/src/components/GridComp.vue
+++ Front/src/components/GridComp.vue
@@ -4,17 +4,20 @@
 </template>
 
 <script setup lang="ts">
-import { defineComponent, ref, onMounted, defineEmits } from 'vue';
+import { ref, onMounted, defineEmits, reactive } from 'vue';
 import { Cell } from '~/types/Cell';
-// import { Piece } from '~/types/Cell'
+import { Piece } from '~/types/Piece'
 import createCell from '~/utils/createCell';
 import { BoardDisposition } from '~/types/BoardDisposition'
+// import BoardImage from "../public/Board.jpg";
 
 const canvas = ref<HTMLCanvasElement | null>(null);
-const canvasWidth = 1200;
-const canvasHeight = 1500;
+const canvasWidth = 960;
+const canvasHeight = 1200;
 const tableRows = 10;
 const tableColumns = 8;
+const cellWidth = canvasWidth / tableColumns;
+const cellHeight = canvasHeight / tableRows;
 const mouseX = ref<number>(0);
 const mouseY = ref<number>(0);
 
@@ -65,59 +68,72 @@ const boardDisposition = {
       "posX": 2,
       "posY": 1,
       "rotation": "NORTH",
-      "class": "com.telegame.code.models.kingolaser.pieces.King"
+      "pieceClass": "com.telegame.code.models.kingolaser.pieces.King"
     },
     {
       "owner": "PLAYER_TWO",
       "posX": 2,
       "posY": 2,
       "rotation": "NORTH",
-      "class": "com.telegame.code.models.kingolaser.pieces.King"
+      "pieceClass": "com.telegame.code.models.kingolaser.pieces.King"
     },
     {
       "owner": "PLAYER_ONE",
       "posX": 7,
       "posY": 2,
       "rotation": "NORTH",
-      "class": "com.telegame.code.models.kingolaser.pieces.Defender"
+      "pieceClass": "com.telegame.code.models.kingolaser.pieces.Defender"
     },
     {
       "owner": "PLAYER_ONE",
       "posX": 7,
       "posY": 9,
       "rotation": "NORTH",
-      "class": "com.telegame.code.models.kingolaser.pieces.Laser"
+      "pieceClass": "com.telegame.code.models.kingolaser.pieces.Laser"
     },
     {
       "owner": "PLAYER_TWO",
       "posX": 0,
       "posY": 0,
       "rotation": "SOUTH",
-      "class": "com.telegame.code.models.kingolaser.pieces.Laser"
+      "pieceClass": "com.telegame.code.models.kingolaser.pieces.Laser"
     },
     {
       "owner": "PLAYER_ONE",
       "posX": 6,
       "posY": 5,
       "rotation": "NORTH",
-      "class": "com.telegame.code.models.kingolaser.pieces.Deflector"
+      "pieceClass": "com.telegame.code.models.kingolaser.pieces.Deflector"
     }
   ]
 }
 
 
+// const backimage = new Image()
+// backimage.src = 'C:/Users/G513/Desktop/DAW/Programacion/King-O-Laser/Front/src/static/kingolaser/Board.jpg'
+
+// const srcs: string[] = ['/Board.jpg'];
+// const images = srcs.map((src) => {
+//     const image = new Image();
+//     image.src = src;
+//     return image;
+// });
+// const imagesLoaded = () => images.every((image) => image.complete);
+
+const board = ref(new Array(tableColumns))
+for (let x = 0; x < board.value.length; x++) {
+  board.value[x] = new Array(tableRows)
+  for (let y = 0; y < tableRows; y++) {
+    board.value[x][y] = createCell(cellWidth, cellHeight, y, x, "#fff")
+  }
+}
+
 
 const drawGrid = (ctx: CanvasRenderingContext2D) => {
-  const cellWidth = canvasWidth / tableColumns;
-  const cellHeight = canvasHeight / tableRows;
 
-  const board = new Array(tableColumns)
-  for (let x = 0; x < board.length; x++) {
-    board[x] = new Array(tableRows)
+  for (let x = 0; x < board.value.length; x++) {
     for (let y = 0; y < tableRows; y++) {
-
-      board[x][y] = createCell(cellWidth, cellHeight, y, x, "#fff")
-      showCell(board[x][y])
+      showCell(board.value[x][y])
     }
   }
 
@@ -133,14 +149,7 @@ const drawGrid = (ctx: CanvasRenderingContext2D) => {
 
 };
 
-onMounted(() => {
-  const ctx = canvas.value?.getContext('2d');
-  if (ctx) {
-    drawGrid(ctx);
-    drawBoard(ctx, boardDisposition)
-  }
 
-});
 
 const emit = defineEmits<{
   (e: 'sendPosition', posY: number, posX: number): void
@@ -151,26 +160,50 @@ const handleClick = (event: MouseEvent) => {
   mouseX.value = Math.floor(event.offsetX / (canvasWidth / tableColumns));
   mouseY.value = Math.floor(event.offsetY / (canvasHeight / tableRows));
 
+  console.log(board.value[mouseY.value][mouseX.value])
 
   emit('sendPosition', mouseY.value, mouseX.value)
 };
 
+// function contains(mouseX: number, mouseY: number, cellWidth: number) {
+//     return this.posX < mouseX && this.posX + cellWidth > mouseX && this.posY < mouseY && this.posY + cellWidth > mouseY
+// }
+
 const drawBoard = (ctx: CanvasRenderingContext2D, boardDisposition: BoardDisposition) => {
-    for (const piece of boardDisposition.board) {
-      ctx.fillStyle = piece.owner === 'PLAYER_ONE' ? 'darkred' : 'darkblue';
-      ctx.fillRect(piece.posX * 150, piece.posY * 150, 150, 150)
-      ctx.font = "30px Arial";
-      ctx.fillStyle = '#fff'
-    ctx.fillText(`Y: ${piece.posY}/ X: ${piece.posX}`, (piece.posX * 150) + 10, (piece.posY * 150) +50)
-    ctx.fillText(`rotation: ${piece.rotation}`, (piece.posX * 150) + 10, (piece.posY * 150) +70)
-    }
+  const pieceList: Piece[] = boardDisposition.board
+  pieceList.forEach(piece => {
+    // piece = new Piece(piece.owner, piece.posY, piece.posX, piece.rotation, piece.pieceClass)
+    board.value[mouseX.value][mouseY.value].empty = false;
+    board.value[mouseX.value][mouseY.value] = new Piece(piece.owner, piece.posY, piece.posX, piece.rotation, piece.pieceClass)
+    ctx.fillStyle = piece.owner === 'PLAYER_ONE' ? 'darkred' : 'darkblue';
+    ctx.fillRect(piece.posX * cellHeight, piece.posY * cellWidth, cellHeight, cellWidth)
+    ctx.font = "20px Arial";
+    ctx.fillStyle = '#fff'
+    ctx.fillText(`Y${piece.posY}:X${piece.posX}`, (piece.posX * cellHeight) + 10, (piece.posY * cellWidth) + 50)
+    ctx.fillText(`rotation: ${piece.rotation}`, (piece.posX * cellHeight) + 10, (piece.posY * cellWidth) + 70)
+  });
+  console.log(board.value)
+}
+
+onMounted(() => {
+  const ctx = canvas.value?.getContext('2d');
+
+  if (ctx) {
+    drawGrid(ctx);
+    // drawBoard(ctx, boardDisposition)
+    // if(imagesLoaded()) {
+    //   ctx.drawImage(images[0], 0, 0)
+    // }
+    // console.log(board.value)
   }
+});
 
 </script>
 
 <style>
 canvas {
-  margin-top: -250px;
-  scale: 0.6;
+  margin-top: -100px;
+  scale: 0.8;
+  /* background-image: url(../public/img/kingolaser/Board.jpg); */
 }
 </style>
