40c2b5fbc6648dd79e95149c1575a57edd28adfe
Eduardo González Lorenzo <edugonzalezlorenzo@gmail.com>
2023-05-22 16:12:55 +0200

    bugfix

---
+++ Back/code/src/main/java/com/telegame/code/models/games/laserboard/LaserBeamService.java
@@ -0,0 +1,32 @@
+package com.telegame.code.models.games.laserboard;
+
+import com.telegame.code.builder.games.laserboard.PieceBuilder;
+import com.telegame.code.models.Board;
+import com.telegame.code.models.games.laserboard.pieces.Bouncer;
+import com.telegame.code.models.games.laserboard.pieces.Piece;
+import com.telegame.code.models.games.laserboard.pieces.PieceSide;
+import com.telegame.code.services.games.LaserBoardService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Service
+public class LaserBeamService {
+
+    private LaserBoardService laserBoardService;
+
+
+
+
+
+}
--- Back/code/src/main/java/com/telegame/code/services/games/LaserBoardService.java
+++ Back/code/src/main/java/com/telegame/code/services/games/LaserBoardService.java
@@ -3,6 +3,7 @@ package com.telegame.code.services.games;
 import com.telegame.code.DTO.games.laserboard.LaserBoardDTO;
 import com.telegame.code.DTO.games.laserboard.PieceDTO;
 import com.telegame.code.builder.games.laserboard.LaserBoardBuilder;
+import com.telegame.code.builder.games.laserboard.PieceBuilder;
 import com.telegame.code.exceptions.InputFormException;
 import com.telegame.code.exceptions.match.MatchInfoException;
 import com.telegame.code.exceptions.match.PieceNotFoundException;
@@ -10,14 +11,15 @@ import com.telegame.code.forms.games.LaserBoardMoveForm;
 import com.telegame.code.models.Board;
 import com.telegame.code.models.GameMatch;
 import com.telegame.code.models.Player;
-import com.telegame.code.models.games.laserboard.LaserBeam;
+import com.telegame.code.models.games.laserboard.Block;
 import com.telegame.code.models.games.laserboard.LaserBoard;
+import com.telegame.code.models.games.laserboard.pieces.Bouncer;
 import com.telegame.code.models.games.laserboard.pieces.Piece;
+import com.telegame.code.models.games.laserboard.pieces.PieceSide;
 import com.telegame.code.repos.BoardRepo;
 import com.telegame.code.repos.games.laserboard.PieceRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.*;
 
 @Service
@@ -72,8 +74,7 @@ public class LaserBoardService {
         piece = piecesList.get(0);
         List<Piece> currentDisposition = pieceRepo.findByLaserBoardId(laserBoard.getId());
 
-        LaserBeam laserBeam = new LaserBeam();
-        Map<String, Object> laserResult = laserBeam.shootLaser(laserBoard, piece.getRotation(), currentDisposition);
+        Map<String, Object> laserResult = shootLaser(laserBoard, piece.getRotation(), currentDisposition);
 
         List<int[]> route = (List<int[]>) laserResult.get("route");
 
@@ -196,5 +197,140 @@ public class LaserBoardService {
         List<Piece> pieceList = pieceRepo.findByPosYAndPosXAndLaserBoardId(posY, posX, laserBoardId);
         Piece pieceToDelete = pieceList.get(0);
         pieceRepo.delete(pieceToDelete);
+    }
+
+    public Map<String, Object> shootLaser(LaserBoard laserBoard, Piece.Direction direction, List<Piece> boardDisposition) {
+
+        Map<String, Object> returnMap = new HashMap<>();
+
+        int[] currentPosition = new int[2];
+        List<int[]> route = new ArrayList<>();
+
+        if (laserBoard.getStatus() == Board.MatchStatus.PLAYER_ONE_TURN) {
+            currentPosition = new int[]{9, 7};
+        } else {
+            currentPosition = new int[]{0, 0};
+        }
+
+        Object[][] board = buildBoard(boardDisposition);
+
+
+        while (currentPosition[0] >= 0 && currentPosition[0] <= 9 &&
+                currentPosition[1] >= 0 && currentPosition[1] <= 7) {
+
+            int[] newYX = forward(direction, currentPosition);
+            int posY = newYX[0];
+            int posX = newYX[1];
+
+
+            if (board[posY][posX] instanceof Piece) {
+                Piece piece = (Piece) board[posY][posX];
+                System.out.println("sides: " + piece.getSides());
+                PieceSide pieceSide = new Block();
+
+                switch (direction) {
+                    case NORTH:
+                        pieceSide = piece.getSide(Piece.Direction.SOUTH);
+                        break;
+                    case EAST:
+                        pieceSide = piece.getSide(Piece.Direction.WEST);
+                        break;
+                    case SOUTH:
+                        pieceSide = piece.getSide(Piece.Direction.NORTH);
+                        break;
+                    case WEST:
+                        pieceSide = piece.getSide(Piece.Direction.EAST);
+                        break;
+                }
+
+                boolean bouncer = (piece instanceof Bouncer);
+
+                Piece.Direction nextDirection = pieceSide.interact(direction, piece.getRotation(), bouncer);
+
+                if (nextDirection == Piece.Direction.STOPPED) {
+                    drawBoard(board);
+                    returnMap.put("message", "BLOCK");
+                    route.add(newYX);
+                    returnMap.put("route", route);
+                    return returnMap;
+                } else if (nextDirection == Piece.Direction.HIT) {
+                    int[] next = forward(direction, currentPosition);
+                    deletePiece(next[0], next[1], laserBoard.getId());
+                    drawBoard(board);
+                    returnMap.put("message", "HIT");
+                    route.add(next);
+                    returnMap.put("route", route);
+                    return returnMap;
+                } else {
+                    direction = nextDirection;
+                    currentPosition = newYX;
+                    route.add(newYX);
+                }
+
+            } else {
+                board[posY][posX] = "  /\\  ";
+                currentPosition[0] = posY;
+                currentPosition[1] = posX;
+                route.add(new int[]{posY, posX});
+                if (posY == 0 && direction == Piece.Direction.NORTH || posY == 9 && direction == Piece.Direction.SOUTH) {
+                    drawBoard(board);
+                    returnMap.put("message", "OUT");
+                    returnMap.put("route", route);
+                    return returnMap;
+                }
+                if (posX == 0 && direction == Piece.Direction.WEST || posX == 7 && direction == Piece.Direction.EAST) {
+                    drawBoard(board);
+                    returnMap.put("message", "OUT");
+                    returnMap.put("route", route);
+                    return returnMap;
+                }
+            }
+        }
+        return returnMap;
+    }
+
+    private int[] forward(Piece.Direction direction, int[] currentPosition) {
+        int posY = currentPosition[0];
+        int posX = currentPosition[1];
+        switch (direction) {
+            case NORTH:
+                posY--;
+                break;
+            case EAST:
+                posX++;
+                break;
+            case SOUTH:
+                posY++;
+                break;
+            case WEST:
+                posX--;
+        }
+        return new int[]{posY, posX};
+    }
+
+    public static Object[][] buildBoard(List<Piece> piecesList) {
+        Object[][] board = new Object[10][8];
+        for (Piece piece : piecesList) {
+            piece = PieceBuilder.buildPiece(piece.getClass().toString(), piece.getOwner(), piece.getPosY(), piece.getPosX(), piece.getRotation());
+            board[piece.getPosY()][piece.getPosX()] = piece;
+        }
+        return board;
+    }
+
+    public static void drawBoard(Object[][] board) {
+        System.out.println("--------------------------------------------------------");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == null) {
+                    board[i][j] = "  " + i + ":" + j + " ";
+                } else if (board[i][j] instanceof Piece) {
+                    Piece piece = (Piece) board[i][j];
+                    board[i][j] = piece.getRotation();
+                }
+                System.out.print(board[i][j] + "|");
+            }
+            System.out.println();
+            System.out.println("--------------------------------------------------------");
+        }
     }
 }
