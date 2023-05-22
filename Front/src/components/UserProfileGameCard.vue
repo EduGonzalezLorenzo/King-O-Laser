10d72fa6b574f5fa209cb44bcad78f3d5b4503aa
Jhota <2002juanan@gmail.com>
2023-05-22 10:58:35 +0200

    Add Seach button

--- Front/src/components/UserProfileGameCard.vue
+++ Front/src/components/UserProfileGameCard.vue
@@ -45,6 +45,15 @@
             >
           </NuxtLink>
           <NuxtLink
+            to="/search-match"
+            class="transition duration-300 hover:bg-gray-200 hover:rounded m-1 p-1 hover:text-gray-600 hover:shadow text-slate-500"
+          >
+            <img
+              src="/img/commonIcon/search.webp"
+              class="w-6 h-6 mx-2"
+            >
+          </NuxtLink>
+          <NuxtLink
             class="transition duration-300 hover:bg-gray-200 hover:rounded m-1 p-1 hover:text-gray-600 hover:shadow text-slate-500 hover:cursor-pointer"
             @click="logout"
           >
