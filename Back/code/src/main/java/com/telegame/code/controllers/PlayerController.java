a97f098b9a750afcae687093e9b777316dba47c4
Eduardo González Lorenzo <edugonzalezlorenzo@gmail.com>
2023-06-15 18:02:52 +0200

    Error in settings added

--- Back/code/src/main/java/com/telegame/code/controllers/PlayerController.java
+++ Back/code/src/main/java/com/telegame/code/controllers/PlayerController.java
@@ -68,8 +68,9 @@ public class PlayerController {
                     .message(playerService.updatePlayerInfo(updatePlayerForm, request.getAttribute("playerName").toString()))
                     .build(), HttpStatus.OK);
         } catch (InputFormException e) {
-
             return new ResponseEntity<>(Message.builder().message("Player form error").build(), HttpStatus.BAD_REQUEST);
+        } catch (PlayerNameException e) {
+            return new ResponseEntity<>(Message.builder().message("You are not registered").build(), HttpStatus.BAD_REQUEST);
         }
     }
 }
